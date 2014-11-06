87118509be42c65204b059712249d1fc4a7e3ff7
Bill DePhillips <bill.dephillips@gmail.com>
2014-11-06 10:35:52 -0800

    ui.router basic PoC

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,20 +13,27 @@ angular
     'ngResource',
     'ngRoute',
     'ngAnimate',
+    'ui.router'
   ])
 
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        action: 'dogs.index',
-        controller: 'dogs.index'
-      })
-      .when('/dogs', {
-        action: 'dogs.index',
-        controller: 'dogs.index'
-      })
-      .when('/dogs/:identifier/edit', {
-        action: 'dogs.edit',
-        controller: 'dogs.edit'
-      });
+
+
+  .config(function ($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise('/state1');
+
+
+    $stateProvider
+      .state('state1',
+      {
+        url: '/state1',
+        template: '<h1>First State</h1>'
+      }
+     )
+     .state('state2',
+     {
+       url: '/state2',
+       template: '<h2>SECOND</h2>'
+     });
+
   });
